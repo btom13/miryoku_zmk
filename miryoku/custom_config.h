7e053378b7396170e7c54f544e8bd5823d219903
btom13 <91095543+btom13@users.noreply.github.com>
2025-05-07 00:42:31 +0000

    add some stuff

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -11,8 +11,8 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-&kp ESC  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp QMARK \
+GRAVE  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
+&kp ESC  K10  K11  K12  K13  U_MT(LA(LC(LSHFT)), G)       U_MT(LA(LC(LSHFT)), M)  K16  K17  K18  K19  &kp QMARK \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp LC(B) \
                K32  U_LT(U_NAV, BSPC)  K34       K35  U_LT(U_NUM, SPACE)  K37
 
